0ff33319d69a59d3078570640192ecf09fc293ac
Pablo <pablolartategui@gmail.com>
2026-04-27 17:25:19 +0200

    feat: Google Tag Manager instalado

--- pages/_document.js
+++ pages/_document.js
@@ -12,8 +12,12 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://tse1.mm.bing.net" />
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5D5D2CH9');` }} />
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5D5D2CH9" height="0" width="0" style="display:none;visibility:hidden"></iframe>` }} />
         <Main />
         <NextScript />
       </body>
